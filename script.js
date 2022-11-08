b95d358d855da54e17513f0f23056a0e127c3b22
CeciliaL1 <cecilia.lepik@hotmail.com>
2022-11-08 17:59:51 +0100

    html-style btn

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+/**
+ * Plus --- Minus knappar input
+ * 
+ * när klickar på minus 
+ * värdet blir en mindre
+ * 
+ * när klickar på plus 
+ * värdet blir en mer 
+ */
+
+const minus = document.querySelectorAll('#minusBtn1, #minusBtn2, #minusBtn3');
+const plus  = document.querySelector('#plusBtn');
+const currentCount = document.querySelector('#value');
+
+
+
+minus.addEventListener('click', subtract);
+plus.addEventListener('click', add);
+
+function subtract(){
+    currentCount.value -= 1;
+    value.style.backgroundColor ='lightgrey';
+}
+
+function add(){
+    currentCount.value = Number(currentCount.value) + 1;
+    value.style.backgroundColor ='lightgrey';
+}
