e581ef2ef7f9b2f4ae00e3cedec090216710ff50
CeciliaL1 <cecilia.lepik@hotmail.com>
2022-11-08 20:15:48 +0100

    html,style,js - inputknappar - funktion

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 
 
+
+
+/**
+ * Plus --- Minus knappar input
+ * 
+ *  OM du trycker på knapp(x)
+ *      värdet blir en mindre
+ * 
+ *  OM annars du trycker på knapp(x)
+ *      värder blir en mer
+ */
+
 function countFunc(x){
-    if (x == 'minusBtn1'){
+    //Pepparkaka
+    if (x == 'minusBtn1'){ 
         const currentCount = document.querySelector('#value1');
         currentCount.value -= 1;
         value1.style.backgroundColor ='lightgrey';
@@ -11,7 +24,8 @@ function countFunc(x){
         currentCount.value = Number(currentCount.value) +1;
         value1.style.backgroundColor ='lightgrey';
     }
-    else if (x == 'minusBtn2'){
+    // Nejlika
+    else if (x == 'minusBtn2'){ 
         const currentCount = document.querySelector('#value2');
         currentCount.value -=1;
         value2.style.backgroundColor ='lightgrey';
@@ -19,7 +33,95 @@ function countFunc(x){
     else if (x == 'plusBtn2'){
         const currentCount = document.querySelector('#value2');
         currentCount.value = Number(currentCount.value) +1;
-        value1.style.backgroundColor ='lightgrey';
+        value2.style.backgroundColor ='lightgrey';
+    }
+    //ingefära
+    else if (x == 'minusBtn3'){ 
+        const currentCount = document.querySelector('#value3');
+        currentCount.value -=1;
+        value3.style.backgroundColor ='lightgrey';
+    }
+    else if (x == 'plusBtn3'){
+        const currentCount = document.querySelector('#value3');
+        currentCount.value = Number(currentCount.value) +1;
+        value3.style.backgroundColor ='lightgrey';
+    }
+    //Vanilj
+    else if (x == 'minusBtn4'){
+        const currentCount = document.querySelector('#value4');
+        currentCount.value -= 1;
+        value4.style.backgroundColor ='lightgrey';
+    }
+    else if (x == 'plusBtn4'){
+        const currentCount = document.querySelector('#value4');
+        currentCount.value = Number(currentCount.value) +1;
+        value4.style.backgroundColor ='lightgrey';
+    }
+    //Choklad
+    else if (x == 'minusBtn5'){
+        const currentCount = document.querySelector('#value5');
+        currentCount.value -= 1;
+        value5.style.backgroundColor ='lightgrey';
+    }
+    else if (x == 'plusBtn5'){
+        const currentCount = document.querySelector('#value5');
+        currentCount.value = Number(currentCount.value) +1;
+        value5.style.backgroundColor ='lightgrey';
+    }
+    //Jordgubb
+    else if (x == 'minusBtn6'){
+        const currentCount = document.querySelector('#value6');
+        currentCount.value -= 1;
+        value6.style.backgroundColor ='lightgrey';
+    }
+    else if (x == 'plusBtn6'){
+        const currentCount = document.querySelector('#value6');
+        currentCount.value = Number(currentCount.value) +1;
+        value6.style.backgroundColor ='lightgrey';
+    }
+      //Hallon
+    else if (x == 'minusBtn7'){
+        const currentCount = document.querySelector('#value7');
+        currentCount.value -= 1;
+        value7.style.backgroundColor ='lightgrey';
+    }
+    else if (x == 'plusBtn7'){
+        const currentCount = document.querySelector('#value7');
+        currentCount.value = Number(currentCount.value) +1;
+        value7.style.backgroundColor ='lightgrey';
+    }
+       // Blue Fizz
+       else if (x == 'minusBtn8'){
+        const currentCount = document.querySelector('#value8');
+        currentCount.value -= 1;
+        value8.style.backgroundColor ='lightgrey';
+    }
+    else if (x == 'plusBtn8'){
+        const currentCount = document.querySelector('#value8');
+        currentCount.value = Number(currentCount.value) +1;
+        value8.style.backgroundColor ='lightgrey';
+    }
+       //Champange
+       else if (x == 'minusBtn9'){
+        const currentCount = document.querySelector('#value9');
+        currentCount.value -= 1;
+        value9.style.backgroundColor ='lightgrey';
+    }
+    else if (x == 'plusBtn9'){
+        const currentCount = document.querySelector('#value9');
+        currentCount.value = Number(currentCount.value) +1;
+        value9.style.backgroundColor ='lightgrey';
+    }
+       //Baconnater
+       else if (x == 'minusBtn10'){
+        const currentCount = document.querySelector('#value10');
+        currentCount.value -= 1;
+        value10.style.backgroundColor ='lightgrey';
+    }
+    else if (x == 'plusBtn10'){
+        const currentCount = document.querySelector('#value10');
+        currentCount.value = Number(currentCount.value) +1;
+        value10.style.backgroundColor ='lightgrey';
     }
 }
 
@@ -32,34 +134,3 @@ function countFunc(x){
 
 
 
-
-
-/**
- * Plus --- Minus knappar input
- * 
- * när klickar på minus 
- * värdet blir en mindre
- * 
- * när klickar på plus 
- * värdet blir en mer 
- 
-
-const minus = document.querySelector('#minusBtn');
-const plus  = document.querySelector('#plusBtn');
-const currentCount = document.querySelector('#value');
-
-
-
-minus.addEventListener('click', subtract);
-plus.addEventListener('click', add);
-
-function subtract(){
-    currentCount.value -= 1;
-    value.style.backgroundColor ='lightgrey';
-}
-
-function add(){
-    currentCount.value = Number(currentCount.value) + 1;
-    value.style.backgroundColor ='lightgrey';
-}
-*/
