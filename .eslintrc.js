3f4afa39c946a87f678ec12fa3de46b08149dfad
CeciliaL1 <cecilia.lepik@hotmail.com>
2022-11-21 08:35:24 +0100

    Adding esLint prettier

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'curly': ['error', 'all'],
+    'indent': ['error', 2, { SwitchCase: 1 }],
+    'max-len': ['error', { code: 120 }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-unreachable': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-undef': process.env.NODE_ENV === 'production' ? 'error' : 'warn',    
+    'vue/max-len': [
+      'error',
+      {
+        code: 120,
+        template: 150,
+      },
+    ],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: {
+          max: 3,
+        },
+        multiline: {
+          max: 3,
+        },
+      },
+    ],
+    'vue/singleline-html-element-content-newline': ['off', {
+      ignoreWhenNoAttributes: true,
+      ignoreWhenEmpty: true,
+      ignores: ['pre', 'textarea'],
+    }],
+};
